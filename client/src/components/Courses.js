226c762469c3e3492e6c1912556a73ab991bd796
Alex Jackson <alexjackson@YG9X70TC7J.local>
2022-12-08 12:56:21 +0000

    Added courses coming soon

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -4,6 +4,8 @@ import Private from '../assets/images/PrivateFull.png';
 import Express from '../assets/images/Express.png';
 import Postnatal from '../assets/images/Postnatal.png';
 import Call from '../assets/images/Call.png';
+import Online from '../assets/images/Online.png';
+import Face from '../assets/images/FacetoFace.png';
 
 export default function Courses() {
   return (
@@ -11,7 +13,12 @@ export default function Courses() {
       <h1 className="courses-header">Find your Hypnobirthing course</h1>
       <div className="courses-wrapper">
         <div className="courses-div">
-          <Grid container spacing={4} sx={{ justifyContent: "center", textAlign: "center" }}>
+        <div className="courses-info">
+        <p>I offer a range of courses that will be suited to you at affordable prices. These courses are science and evidence based. They focus on the data and the facts!</p>
+        <p>Participating in these courses will mean you receive a workbook as well as other useful resources to use during the course. It will also include other useful information / learning resources for after birth!</p>
+        <p>Here are some details but feel free to get in touch with any questions or if you’d like something more flexible.</p>
+      </div>
+          <Grid container spacing={4} sx={{ justifyContent: "center", textAlign: "center", paddingBottom: "20px" }}>
           <Grid item xs={12} sm={6} md={4} lg={3}>
             <div class="flip-card">
               <div class="flip-card-inner">
@@ -25,7 +32,7 @@ export default function Courses() {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <div class="flip-card">
               <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -44,7 +51,7 @@ export default function Courses() {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <div class="flip-card">
               <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -56,7 +63,7 @@ export default function Courses() {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={4} lg={3}>
             <div class="flip-card">
               <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -68,6 +75,14 @@ export default function Courses() {
               </div>
             </div>
           </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={6}>
+              <img src={Online} alt="Full Group Online - coming soon" className="course-options" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <img src={Face} alt="Full Group Face to Face - coming soon" className="course-options" />
+            </Grid>
           </Grid>
           {/* <ul className="course-list">
             <li>Private Full Course</li>
