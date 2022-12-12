3421e90c970ed104bcc2d47dac13398517bf583b
Alex Jackson <alexjackson@yg9x70tc7j.home>
2022-12-12 17:26:01 +0000

    Added image of instagram feed and fade effects

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,15 +14,15 @@ export default function Footer() {
     <Fade bottom>
     <div className='footer'>
         <Grid container spacing={2}>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={4} md={4}>
         <img src={Logo} alt="Mighty Fierce logo" />
         </Grid>
-        <Grid item xs={4}>
-        <p><a href="https://www.instagram.com/mightyfiercebirth/" target="_blank"><InstagramIcon fontSize="large" sx={{color: "white"}} /></a> @mightyfiercebirth</p>
-                <p><a href="https://m.facebook.com/mightyfiercebirth/" target="_blank"><FacebookIcon fontSize="large" sx={{color: "white"}} /></a> Mighty Fierce Birth</p>
-                <p><PhoneAndroidIcon fontSize="large" sx={{color: "white"}} /> 07519 954715</p>
+        <Grid item xs={4} sm={4} md={4}>
+          <p><a href="https://www.instagram.com/mightyfiercebirth/" target="_blank"><InstagramIcon fontSize="large" sx={{color: "white"}} /></a> @mightyfiercebirth</p>
+          <p><a href="https://m.facebook.com/mightyfiercebirth/" target="_blank"><FacebookIcon fontSize="large" sx={{color: "white"}} /></a> Mighty Fierce Birth</p>
+          <p><PhoneAndroidIcon fontSize="large" sx={{color: "white"}} /> 07519 954715</p>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={4} md={4}>
         <NavLink to="/t&cs" className="tcs-link" style={{ color: "white", fontFamily: "League Spartan", listStyleType: "none", fontSize: "20px", justifyContent: "center", textAlign: "center" }}><li className="tcs-link">Terms & Conditions</li></NavLink>
         </Grid>
         </Grid>
--- client/src/components/Instagram.js
+++ client/src/components/Instagram.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { InstagramEmbed } from 'react-social-media-embed';
 import "@ptkdev/webcomponent-instagram-widget";
+import Insta from '../assets/images/Insta.png';
+import { NavLink } from 'react-router-dom';
+import { Fade } from 'react-reveal';
 
 
 
 export default function Instagram() {
   return (
     <div className=''>
+      <Fade bottom>
         <h1 className='socials-header'>Follow me:<br />
         @MightyFierceBirth</h1>
-        <instagram-widget username="@mightyfiercebirth" grid="3x3"></instagram-widget>
-        <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: "50px" }}>
-            {/* <InstagramEmbed url="https://www.instagram.com/p/CkmOn9cjvXs/"  /> */}
-            {/* <InstagramEmbed url="https://www.instagram.com/p/CkmGxXfD3HN/" width={328} />
-            <InstagramEmbed url="https://www.instagram.com/p/Cj3jwdAq09w/" width={328} /> */}
+        <div>
+          <a href="https://www.instagram.com/mightyfiercebirth/" target="_blank"><img src={Insta} alt="Instagram feed - mighty fierce birth" className="feed" /></a>
         </div>
+        {/* <instagram-widget username="@mightyfiercebirth" grid="3x3"></instagram-widget>
+        <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: "50px" }}>
+            <InstagramEmbed url="https://www.instagram.com/p/CkmOn9cjvXs/"  />
+            <InstagramEmbed url="https://www.instagram.com/p/CkmGxXfD3HN/" width={328} />
+            <InstagramEmbed url="https://www.instagram.com/p/Cj3jwdAq09w/" width={328} />
+        </div> */}
+        </Fade>
     </div>
   )
 }
