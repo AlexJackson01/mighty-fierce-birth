3912855dabe2ec8f37403a6cc52009a978aa369c
AlexJackson01 <ajjackson22@gmail.com>
2023-01-06 16:18:25 +0000

    Improved Contact page on mobile

--- client/src/screens/ContactPage.js
+++ client/src/screens/ContactPage.js
@@ -15,7 +15,7 @@ export default function ContactPage () {
           <h2>Contact</h2>
           <Grid container spacing={2} sx={{ paddingBottom: 3 }}>
             <Grid item xs={12} sm={6} md={6}>
-              <Paper elevation={3} sx={{ padding: '50px', height: 520 }}>
+              <Paper elevation={3} sx={{ padding: '30px' }}>
                 <img
                   src={Photo}
                   alt='image of founder, Prosha and her daughter'
@@ -27,23 +27,22 @@ export default function ContactPage () {
             <Grid item xs={12} sm={6} md={6}>
               <Paper
                 elevation={3}
-                sx={{ padding: '50px', paddingTop: 15, height: 520 }}
+                sx={{ padding: '30px', display: "flex", justifyContent: "center", alignItems: "center", height: 436 }}
               >
                 <Socials />
               </Paper>
             </Grid>
           </Grid>
 
-          <Grid container spacing={2}>
+          <Grid container spacing={0}>
             <Grid item xs={12}>
-              <Paper elevation={3} sx={{ padding: '50px' }}>
+              <Paper elevation={3} sx={{ marginTop: "-8px", padding: '30px' }}>
                 <Form />
               </Paper>
             </Grid>
           </Grid>
         </Fade>
       </div>
-      <Footer />
     </div>
   )
 }
